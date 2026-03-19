a444c79bc32aaa635b899cdeca47090196f07977
fayaz-Hussain <fayazhussain.cs@gmail.com>
2026-03-20 02:50:27 +0500

    Actually saving the modal and uploader fixes

--- fyndbox-frontend/src/components/Modal/EntityActionModal.styles.ts
+++ fyndbox-frontend/src/components/Modal/EntityActionModal.styles.ts
@@ -1,5 +1,6 @@
 import { Box, Button, IconButton, styled, Typography } from '@mui/material';
 
+// Modal layout styles.
 export const ModalContainer = styled(Box)(({}) => ({
   position: 'fixed',
   top: 0,
@@ -104,11 +105,39 @@ export const StepperButton = styled(Button)(({ theme }) => ({
 
 export const ActionButtonsContainer = styled(Box)(({ theme }) => ({
   display: 'flex',
-  padding: theme.spacing(2, 0),
+  flexDirection: 'row',
+  gap: theme.spacing(2),
+  padding: theme.spacing(3, 0),
   position: 'sticky',
   bottom: 0,
   backgroundColor: theme.palette.secondary.main,
   zIndex: 10,
   justifyContent: 'center',
   alignItems: 'center',
+}));
+
+const EntityActionButton = styled(Button)(({ theme }) => ({
+  borderRadius: '8px',
+  padding: theme.spacing(1, 4),
+  textTransform: 'none',
+  fontSize: '1.25rem',
+  minWidth: '120px',
+}));
+
+export const DeleteActionButton = styled(EntityActionButton)(({ theme }) => ({
+  backgroundColor: 'transparent',
+  color: theme.palette.error.main,
+  border: `1px solid ${theme.palette.error.light}`,
+  '&:hover': {
+    backgroundColor: 'rgba(175, 87, 87, 0.04)',
+    border: `1px solid ${theme.palette.error.main}`,
+  },
+}));
+
+export const SaveActionButton = styled(EntityActionButton)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  '&:hover': {
+    backgroundColor: theme.palette.primary.dark,
+  },
 }));
